970a319662030f66b934fb7c07fd9d6879b62139
AyanNaska <ayannaskarind@gmail.com>
2022-11-15 21:53:11 +0530

    It is a Digital Twin for UR5e robot

---
+++ Assets/Scripts/UR3/Link/ur3_link3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class ur3_link3 : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        try
+        {
+            transform.localEulerAngles = new Vector3(0.0f, 0.0f, (float)(ur_data_processing.UR_Stream_Data.J_Orientation[2] * (180.0 / Math.PI)));
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Exception:" + e);
+        }
+    }
+    void OnApplicationQuit()
+    {
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/UR3/Link/ur3_link5.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class ur3_link5 : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        try
+        {
+            transform.localEulerAngles = new Vector3(0.0f, (-1.0f) * (float)(ur_data_processing.UR_Stream_Data.J_Orientation[4] * (180.0 / Math.PI)), 0f);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Exception:" + e);
+        }
+    }
+    void OnApplicationQuit()
+    {
+        Destroy(this);
+    }
+}
